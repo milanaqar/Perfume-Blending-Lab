df219f1f344781929a91efc5fc546ce64e719bbf
Anka <milanaqar@gmail.com>
2026-05-19 04:16:11 +0300

    in progress choice_perfume

--- perfume.cpp
+++ perfume.cpp
@@ -1,7 +1,8 @@
 // Perfume Blending System
+
 #include <iostream>
 #include <string>
-#include <algorithm>
+#include <algorithm>   // for min function
 using namespace std;
 
 int main_menu()
@@ -30,7 +31,7 @@ void start_stocks()
 
 }
 
-void max_production()
+int max_production()
 {
     // stock 
     int alcohol = 1000;
@@ -63,10 +64,48 @@ void max_production()
     cout << "Vanilla Dream: " << max_vanilla << " bottles\n";
     cout << "Citrus Breeze: " << max_citrus << " bottles\n";
     cout << "--------------------------------\n";
+
+    return max_rose, max_vanilla, max_citrus;
     
 }
 
-void 
+int choice_perfume(int max_rose)
+{
+    cout << "\n--- CREATE PERFUME ---\n";
+    cout << "1. Rose Mist\n";
+    cout << "2. Vanilla Dream\n";
+    cout << "3. Citrus Breeze\n";
+    cout << "------------------\n";
+
+    int choice_perfume;
+    cout << "Enter choice: ";
+    cin >> choice_perfume;
+
+    cout << "Enter quantity (number of bottles): ";
+    int quantity;
+    cin >> quantity;
+
+    switch (choice_perfume)
+    {
+        case 1:
+            cout << "Creating " << quantity << " bottles of Rose Mist...\n";
+           
+            if (quantity > max_rose)
+            {
+                cout << "Not enough stock to create " << quantity << " bottles of Rose Mist. Maximum possible: " << max_rose << " bottles.\n";
+            } 
+            break;
+        case 2:
+            cout << "Creating " << quantity << " bottles of Vanilla Dream...\n";
+            break;
+        case 3:
+            cout << "Creating " << quantity << " bottles of Citrus Breeze...\n";
+            break;
+        default:
+            cout << "Invalid choice. Returning to main menu.\n";
+    }
+
+}
 
 int main()
 {
@@ -109,13 +148,14 @@ int main()
     while (true)
     {
         int choice = main_menu();
+        int max_rose = max_production();
 
         switch (choice)
         {
             case 1:
                 start_stocks();
                 break;
-            case 2:
+            case 2:                       // error : showing max production without need 
                 cout << "\n--- RECIPES ---\n";
                 cout << "Rose Mist: Αλκοόλη 20 ml, Άρωμα Τριαντάφυλλου 10 ml, Μπουκάλι 1.\n";
                 cout << "Vanilla Dream: Vanilla Dream 25ml, Άρωμα Βανίλιας 12 ml, Μπουκάλι 1.\n";
@@ -123,8 +163,8 @@ int main()
                 cout << "------------------\n";
                 break;
             case 3:
-                max_production();
-                
+                max_production();       // error: showing max production two times
+                choice_perfume(max_rose);
                 break;
             case 4:
                 cout << "Exiting program. Goodbye!\n";
